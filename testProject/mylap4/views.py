cf17edb0cb04ada573830aa690a2fa631a301cf6
aseel-uni <442200868@student.ksu.edu.sa>
2024-03-06 14:36:54 +0300

    final

--- testProject/mylap4/views.py
+++ testProject/mylap4/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+# first page
 def default(request):
-    return HttpResponse("<h1>This is a site to calculate tax</h1>")
+    return HttpResponse("<h1> this is a site to calculate tax </h1>")
 
-
+#second page
 def calculate_tax(request, price):
     tax_rate = 0.15
     total_price = price + ( price * tax_rate )
     return HttpResponse( f"<h1>The total price after tax is: {total_price} </h1> " )
 
+#third page
 def tax_rate(request):
 
     tax_rate = 15
